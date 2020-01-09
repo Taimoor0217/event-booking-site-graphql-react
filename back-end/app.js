ad9d018bd99135b55063953d8049754f37457a12
taimoor0217 <taimoor0217@gmail.com>
2020-01-10 03:59:32 +0500

    Add Moongoose model for event and update create event mutation

--- back-end/app.js
+++ back-end/app.js
@@ -4,9 +4,10 @@ const graphqlhttp = require('express-graphql');
 const {buildSchema} = require('graphql')
 const mongoose = require('mongoose')
 const app = express();
-
-let CurrentEvents = [];
 app.use(bodyParser.json());
+// let CurrentEvents = [];
+const Event = require('./models/event')
+
 app.get('/' , (req , res , next)=>{
     res.send('Hello World')
 })
@@ -47,19 +48,32 @@ app.use('/graphql' , graphqlhttp({
             return CurrentEvents;
         },
         createEvent: function(args){
-            const data = {
-                ...args.eventInput,
-                _id : Math.random().toString()
-            }
-            CurrentEvents.push(data)
-            return args.eventInput
+            // console.log(args.eventInput.name)
+            const event = new Event({
+                name: args.eventInput.name,
+                description: args.eventInput.description,
+                price: args.eventInput.price,
+                attendes: args.eventInput.attendes,
+                // _id : Math.random().toString(),
+                date: new Date(args.eventInput.date)
+            })
+            event.save()
+            .then((result)=>{
+                console.log(result);
+                return { ...result._doc };
+            })
+            .catch((err) => {
+                console.log(err)
+                throw err; 
+            })
+            // CurrentEvents.push(data)
         }
     },
-    //for in browser graohql intrface, set this false on production mode
+    //for in browser graohql intrface, set this false on production mode    
     graphiql: true
 }));
 // console.log(process.env.db_admin , process.env.db_password)
-mongoose.connect(`mongodb+srv://${process.env.db_admin}:${process.env.db_password}@cluster0-taeyx.mongodb.net/test?retryWrites=true&w=majority` , { useNewUrlParser: true ,  useUnifiedTopology: true  })
+mongoose.connect(`mongodb+srv://${process.env.db_admin}:${process.env.db_password}@cluster0-taeyx.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority` , { useNewUrlParser: true ,  useUnifiedTopology: true  })
 .then(()=>{
     app.listen(8000 , ()=> console.log('🚀 Server Running on Port 8000...'))
 }).catch(err=>{
---
+++ back-end/models/event.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const eventSchema = new Schema({
+    // event:{
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        attendes: {
+            type: Number,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        }
+    // }
+});
+module.exports = mongoose.model('Event', eventSchema)
+// might be a problem with this export syntax
